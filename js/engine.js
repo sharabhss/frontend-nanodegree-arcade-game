1dc651d90747a2375251e7d6b3cb13b4273facbc
sharabhss <sharabhs93@gmail.com>
2017-08-01 10:48:47 -0400

    activated more player characters

--- js/engine.js
+++ js/engine.js
@@ -171,7 +171,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-horn-girl.png'
+        'images/char-horn-girl.png',
+        'images/char-cat-girl.png',
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
